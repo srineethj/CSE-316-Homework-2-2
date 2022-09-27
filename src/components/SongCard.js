04bb2075ba4abb5f988ff2a5a1c48446af1d996f
srineethj <50963073+srineethj@users.noreply.github.com>
2022-09-26 20:34:00 -0400

    Added song enumeration and linking

--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -77,7 +77,10 @@ export default class SongCard extends React.Component {
                 onDrop={this.handleDrop}
                 draggable="true"
             >
-                {song.title} by {song.artist}
+                {num}{"."}{" "}
+                <a href={'https://www.youtube.com/watch?v=' + song.youTubeId} target='blank'>
+                    {song.title} by {song.artist}
+                </a>
             </div>
         )
     }
